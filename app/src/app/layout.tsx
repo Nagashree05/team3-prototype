4dab941dce3c7d230315109ae73cd6530d77c539
Mohit <themohitnair@protonmail.com>
2025-01-24 16:48:52 +0530

    NextJS Boilerplate added along with FastAPI dummy endpoint

---
+++ app/src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const interFont = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Discreta",
+  description: "Discrete Mathematics Made Easy",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${interFont.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
